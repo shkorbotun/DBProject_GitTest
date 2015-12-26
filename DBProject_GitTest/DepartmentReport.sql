9ee8a59e3d929c7dde98ec25279ac95de699b3d0
user <user>
2015-12-26 02:39:52 -0800

    [DB] modified DepartmentReport by User 1

--- DBProject_GitTest/DepartmentReport.sql
+++ DBProject_GitTest/DepartmentReport.sql
@@ -1,7 +1,7 @@
 USE AdventureWorks2008R2
 GO
 SELECT		[GroupName]
-			,count(*) As TotalDepartments
+			,count(*) As DepartmentNumber
 FROM		[HumanResources].[Department]
 GROUP BY	[GroupName]
 ORDER BY	[GroupName]
