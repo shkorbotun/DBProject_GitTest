c2f06ec16d1c9b028e8c9fdc9ee8a4728c45d06f
Alex Shkorbotun <shkorbotun@gmail.com>
2015-12-26 03:10:05 -0800

    [DB] renamed column in DepartmentReport by User 1

--- DBProject_GitTest/DepartmentReport.sql
+++ DBProject_GitTest/DepartmentReport.sql
@@ -1,7 +1,7 @@
 USE AdventureWorks2008R2
 GO
 SELECT		[GroupName]
-			,count(*) As DepartmentNumber
+			,count(*) As Departments
 FROM		[HumanResources].[Department]
 GROUP BY	[GroupName]
 ORDER BY	DepartmentNumber
