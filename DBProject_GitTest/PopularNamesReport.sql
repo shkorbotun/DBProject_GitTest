2b14be7257c4d9a9f11a1236727011cb88a8691d
Alex Shkorbotun <shkorbotun@gmail.com>
2015-12-27 02:33:14 -0800

    [DB] changed condition in PopularNameReport by User 1

--- DBProject_GitTest/PopularNamesReport.sql
+++ DBProject_GitTest/PopularNamesReport.sql
@@ -6,8 +6,19 @@ SELECT	[FirstName]
 		,COUNT(*) As TotalNumber
 FROM	[HumanResources].[vEmployee]
 GROUP BY [FirstName]
-)
-SELECT	top 10 
-		*
+),
+condition as
+(SELECT distinct TotalNumber 
+ FROM core_set)
+ 
+SELECT	*
 FROM	core_set
+WHERE	TotalNumber in
+
+(
+SELECT	TOP 5
+		TotalNumber
+FROM	condition
+ORDER BY TotalNumber desc
+)
 ORDER BY TotalNumber desc
