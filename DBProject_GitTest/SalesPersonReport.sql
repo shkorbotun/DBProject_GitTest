8dda163196b69d9f3ae94b298c5003d6b13fd21f
Alex Shkorbotun <shkorbotun@gmail.com>
2015-12-27 09:38:13 -0800

    [DB] added rank feature in SalesPersonReport by User 1

--- DBProject_GitTest/SalesPersonReport.sql
+++ DBProject_GitTest/SalesPersonReport.sql
@@ -7,9 +7,13 @@ SELECT	 [SalesTerritory]
 		,[2006]
 		,[2007]
 		,[2008]
+		,DENSE_RANK() OVER (partition by [SalesTerritory] order by [2006] desc) As Rank_2006
+		,DENSE_RANK() OVER (partition by [SalesTerritory] order by [2007] desc) As Rank_2007
+		,DENSE_RANK() OVER (partition by [SalesTerritory] order by [2008] desc) As Rank_2008
+		,[2006] + [2007] + [2008] As Total
+		,DENSE_RANK() OVER (partition by [SalesTerritory] order by ([2006] + [2007] + [2008]) desc) As Rank_Total
 
 FROM	[Sales].[vSalesPersonSalesByFiscalYears]
 
 ORDER BY	 [SalesTerritory]
-			,[JobTitle]
-			,[FullName]
+			,Total desc
