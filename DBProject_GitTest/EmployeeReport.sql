43e164eb99097f4a083856af4f8e0b53e1bb0dfe
Alex Shkorbotun <shkorbotun@gmail.com>
2015-12-28 09:43:23 -0800

    [DB] developed Employeereport by User 2

---
+++ DBProject_GitTest/EmployeeReport.sql
@@ -0,0 +1,16 @@
+USE [AdventureWorks2008R2]
+GO
+WITH core_set as
+(
+SELECT	[JobTitle]
+		,COUNT(*) As Total
+
+FROM	[HumanResources].[vEmployee]
+GROUP BY [JobTitle]
+)
+
+SELECT	ROW_NUMBER() over(order by [JobTitle])
+		,[JobTitle]
+		,Total
+		,ROUND(cast(Total as float)/(SELECT SUM(Total) FROM core_set)*100,1) As ShareOfTotal
+FROM	core_set
--- DBProject_GitTest/EmployeeReportDraft.sql
+++
@@ -1,9 +0,0 @@
-USE [AdventureWorks2008R2]
-GO
-
-SELECT	 [JobTitle]
-		,COUNT(*) As Total
-
-FROM	[HumanResources].[vEmployee]
-GROUP BY [JobTitle]
-ORDER BY Total desc
