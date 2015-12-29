82f69a6dfeea1bd27353c48255b0281a84cd7e77
Alex Shkorbotun <shkorbotun@gmail.com>
2015-12-29 05:36:58 -0800

    [DB] added >2 condition to VendorReport by User 1

--- DBProject_GitTest/VendorReport.sql
+++ DBProject_GitTest/VendorReport.sql
@@ -4,7 +4,9 @@ GO
 SELECT	 StateProvinceName As State
 		,COUNT(City) As Vendors
 		,COUNT(distinct City) As City
+		,CAST(COUNT(City) as float)/COUNT(distinct City)
 
 FROM	[Purchasing].[vVendorWithAddresses]
 GROUP BY StateProvinceName
+HAVING CAST(COUNT(City) as float)/COUNT(distinct City) >1
 ORDER BY Vendors desc
