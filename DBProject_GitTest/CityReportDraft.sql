ce3a0007881cfdf75945d8c76c2027bc08c04c09
Alex Shkorbotun <shkorbotun@gmail.com>
2015-12-29 02:29:54 -0800

    [DB] added statistics in CityReport by User 2

--- DBProject_GitTest/CityReportDraft.sql
+++ DBProject_GitTest/CityReportDraft.sql
@@ -1,10 +1,53 @@
 USE [AdventureWorks2008R2]
 GO
+
+;WITH core_set as 
+(
+SELECT	 [CountryRegionName]
+		,[City]
+		,COUNT(*) As TotalCities
+FROM	 [Sales].[vStoreWithAddresses]
+GROUP BY [CountryRegionName]
+		,[City]
+),
+grouping_set As 
+(
+SELECT	CountryRegionName
+		,TotalCities As Stores
+		,COUNT(*) As Cities
+FROM	core_set
+GROUP BY CountryRegionName
+		,TotalCities
+),
+combined_set As 
+(
+SELECT	CountryRegionName
+		,Stores
+		,QUOTENAME(CAST(Stores as varchar(3))+'-'+CAST(Cities as varchar(3)),'[]') As Stores_Cities
+FROM	grouping_set
+),
+
+total_set As
+(
 SELECT	 [CountryRegionName]
 		,COUNT([City]) As TotalStores
 		,COUNT(distinct [City]) As TotalCities
 FROM	 [Sales].[vStoreWithAddresses]
 GROUP BY [CountryRegionName]
-ORDER BY [CountryRegionName] desc
+)
 
-
+SELECT	CS.CountryRegionName
+		,TotalStores
+		,TotalCities
+		,CAST(
+		 (SELECT	
+					Stores_Cities + ' '
+		  FROM		combined_set
+		  WHERE		CountryRegionName = CS.CountryRegionName
+		  ORDER BY	Stores
+		  FOR XML PATH (''), TYPE)
+		  As NVARCHAR(max)
+		  )
+		  As Stores_Cities
+		  
+FROM	total_set As CS
